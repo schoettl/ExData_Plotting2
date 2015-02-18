c104f1094449dc295e9dfd46b181c0ee13f4d47e
Jakob Schöttl <jschoett@gmail.com>
2015-02-18 23:23:25 +0100

    Bugfix: forgot comma after param

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ emissions = aggregate(baltimore_city_data$Emissions, list(year=baltimore_city_da
 
 png("plot2.png")
 with(emissions, plot(year, x,
-  main="Total emissions in Baltimore City"
+  main="Total emissions in Baltimore City",
   ylab="total emissions (in tons)"))
 dev.off()
