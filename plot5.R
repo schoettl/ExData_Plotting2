7429a96478654bf2bdf3b3adccde9e8940ea602d
Jakob Schöttl <jschoett@gmail.com>
2015-02-19 11:41:16 +0100

    Added code for plot 5 and 6

--- plot5.R
+++ plot5.R
@@ -3,8 +3,24 @@ library(ggplot2)
 codes = readRDS("Source_Classification_Code.rds")
 data  = readRDS("summarySCC_PM25.rds")
 
-motor_vehicles_codes = codes$SCC[grep("Mobile Sources", codes$SCC.Level.One)]
-emissions = subset(data, fips == "24510" & (SCC %in% motor_vehicles_codes))
+get_motor_vehicle_codes = function(codes) {
+  
+  # Select level 1 from: levels(codes$SCC.Level.One)
+  level1 = c("Internal Combustion Engines", "Storage and Transport", "Mobile Sources")
+  
+  # Select level 2 from: subset_at_lev1 = subset(codes, SCC.Level.One %in% level1); levels(factor(subset_at_lev1$SCC.Level.Two))
+  level2 = c("Engine Testing", "Highway Vehicles - Diesel", "Highway Vehicles - Gasoline", "Off-highway 2-stroke Gasoline Engines",
+      "Off-highway 4-stroke Gasoline Engines", "Off-highway Diesel Engines", "Off-highway LPG-fueled Engines", "Off-highway Vehicle Diesel",
+      "Off-highway Vehicle Gasoline, 2-Stroke", "Off-highway Vehicle Gasoline, 4-Stroke", "Unpaved Roads")
+  
+  subset_at_lev12 = subset(codes, SCC.Level.One %in% level1 & SCC.Level.Two %in% level2)
+  matching_at_lev3 = grep("auto|gasoline|vehicle|road", subset_at_lev12$SCC.Level.Three, ignore.case=TRUE)
+  
+  subset_at_lev12[matching_at_lev3, "SCC"]
+}
+
+motor_vehicle_codes = get_motor_vehicle_codes(codes)
+emissions = subset(data, fips == "24510" & (SCC %in% motor_vehicle_codes))
 
 png("plot5.png")
 ggplot(emissions, aes(year, Emissions)) +
--- plot6.R
+++ plot6.R
@@ -3,13 +3,28 @@ library(ggplot2)
 codes = readRDS("Source_Classification_Code.rds")
 data  = readRDS("summarySCC_PM25.rds")
 
-fips_bc  = 24510 # Baltimore City
-fips_lac = 06037 # Los Angeles County
+get_motor_vehicle_codes = function(codes) {
+  
+  # Select level 1 from: levels(codes$SCC.Level.One)
+  level1 = c("Internal Combustion Engines", "Storage and Transport", "Mobile Sources")
+  
+  # Select level 2 from: subset_at_lev1 = subset(codes, SCC.Level.One %in% level1); levels(factor(subset_at_lev1$SCC.Level.Two))
+  level2 = c("Engine Testing", "Highway Vehicles - Diesel", "Highway Vehicles - Gasoline", "Off-highway 2-stroke Gasoline Engines",
+      "Off-highway 4-stroke Gasoline Engines", "Off-highway Diesel Engines", "Off-highway LPG-fueled Engines", "Off-highway Vehicle Diesel",
+      "Off-highway Vehicle Gasoline, 2-Stroke", "Off-highway Vehicle Gasoline, 4-Stroke", "Unpaved Roads")
+  
+  subset_at_lev12 = subset(codes, SCC.Level.One %in% level1 & SCC.Level.Two %in% level2)
+  matching_at_lev3 = grep("auto|gasoline|vehicle|road", subset_at_lev12$SCC.Level.Three, ignore.case=TRUE)
+  
+  subset_at_lev12[matching_at_lev3, "SCC"]
+}
 
-motor_vehicles_codes = codes$SCC[grepl("Mobile Sources", codes$SCC.Level.One) & grepl("[Vv]ehicle", codes$Short.Name)]
-emissions = subset(data, (fips == fips_bc | fips == fips_lac) & (SCC %in% motor_vehicles_codes))
+fips_bc  = "24510" # Baltimore City
+fips_lac = "06037" # Los Angeles County
+motor_vehicle_codes = get_motor_vehicle_codes(codes)
+emissions = subset(data, (fips %in% c(fips_bc, fips_lac)) & (SCC %in% motor_vehicle_codes))
 
-# Any nicer solution? The switch function is for length 1 vector as EXPR only.
+# Any nicer solution? The switch function is only for length 1 vector as EXPR.
 emissions$county = NA
 emissions$county[emissions$fips == fips_bc] = "Baltimore City"
 emissions$county[emissions$fips == fips_lac] =  "Los Angeles County"
