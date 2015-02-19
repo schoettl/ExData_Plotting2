517ca8c5aeb414c3db7927ac07903c230172fbaa
Jakob Schöttl <jschoett@gmail.com>
2015-02-19 09:07:57 +0100

    Write all fips as string

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 data = readRDS("summarySCC_PM25.rds")
 
-baltimore_city_data = subset(data, fips == 24510)
+baltimore_city_data = subset(data, fips == "24510")
 emissions = aggregate(baltimore_city_data$Emissions, list(year=baltimore_city_data$year), sum)
 
 png("plot2.png")
--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@ library(ggplot2)
 
 data = readRDS("summarySCC_PM25.rds")
 
-baltimore_city_data = subset(data, fips == 24510)
+baltimore_city_data = subset(data, fips == "24510")
 emissions = aggregate(baltimore_city_data$Emissions, baltimore_city_data[, c("year", "type")], sum)
 
 png("plot3.png")
